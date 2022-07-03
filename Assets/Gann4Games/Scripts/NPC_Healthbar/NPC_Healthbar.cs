2788c80b6068ec6b4de273fbcfa9dc391d0995d1
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2022-07-02 22:56:06 -0300

    Update NPC_Healthbar.cs
    Still changes need to be done, for example update the values only when the NPC is damaged or display a color based on the damage severity.

--- Assets/Gann4Games/Scripts/NPC_Healthbar/NPC_Healthbar.cs
+++ Assets/Gann4Games/Scripts/NPC_Healthbar/NPC_Healthbar.cs
@@ -2,30 +2,26 @@
 
 public class NPC_Healthbar : MonoBehaviour
 {
-    public float height = .7f;
-    public GameObject healthbarPrefab;
-    public TMPro.TextMeshPro textField;
+    [SerializeField] private float height = .7f;
+    [SerializeField] private GameObject healthbarPrefab;
 
-    private Animator _anim;
+    private TMPro.TextMeshPro _textField;
+    private Animator _animator;
     private CharacterCustomization _character;
 
-    private void OnDrawGizmosSelected()
-    {
-        Debug.DrawLine(transform.position, transform.position + Vector3.up * height);
-    }
+    private Vector3 followPosition => transform.position + Vector3.up * height;
+
     private void Start()
     {
         _character = GetComponent<CharacterCustomization>();
-        healthbarPrefab = Instantiate(healthbarPrefab, transform.position + Vector3.up*height, transform.rotation);
+        healthbarPrefab = Instantiate(healthbarPrefab, transform.position + Vector3.up * height, transform.rotation);
 
-        textField = healthbarPrefab.GetComponentInChildren<TMPro.TextMeshPro>();
-
-        _anim = healthbarPrefab.GetComponent<Animator>();
-        healthbarPrefab.transform.parent = transform;
+        _textField = healthbarPrefab.GetComponentInChildren<TMPro.TextMeshPro>();
+        _animator = healthbarPrefab.GetComponent<Animator>();
     }
     private void Update()
     {
-        if(_anim) _anim.SetFloat("fill", _character.HealthController.HealthPercentage);
-        textField.text = string.Format("{0}", _character.preset.character_name);
+        if(_animator) _animator.SetFloat("fill", _character.HealthController.HealthPercentage);
+        _textField.text = string.Format("{0}", _character.preset.character_name);
     }
 }
