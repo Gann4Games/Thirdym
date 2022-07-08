f70f82e808b6b008bedbc32e0928d136519b4f3a
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2022-07-07 21:35:19 -0300

    Add defibrilator model

---
+++ Assets/Gann4Games/Scripts/State Machines/Npc States/NpcMedicState.cs
@@ -0,0 +1,20 @@
+namespace Gann4Games.Thirdym.StateMachines
+{
+    public class NpcMedicState : IState
+    {
+        public void OnEnterState(StateMachine context)
+        {
+            throw new System.NotImplementedException("Medic state has not been done.");
+        }
+
+        public void OnExitState(StateMachine context)
+        {
+            
+        }
+
+        public void OnUpdateState(StateMachine context)
+        {
+            
+        }
+    }
+}
